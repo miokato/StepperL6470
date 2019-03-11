1f2629dafefc0def890486115f7ddf95a0446252
miokato <miokato@me.com>
2019-03-11 17:37:46 +0900

    First commit

---
+++ StepperL6470.cpp
@@ -0,0 +1,267 @@
+#include <StepperL6470.h>
+#include <arduino.h>
+#include <SPI.h>
+
+void L6470::setparam_abspos(long val) { this->transfer(0x01, 3, val); }
+void L6470::setparam_elpos(long val) { this->transfer(0x02, 2, val); }
+void L6470::setparam_mark(long val) { this->transfer(0x03, 3, val); }
+void L6470::setparam_acc(long val) { this->transfer(0x05, 2, val); }
+void L6470::setparam_dec(long val) { this->transfer(0x06, 2, val); }
+void L6470::setparam_maxspeed(long val) { this->transfer(0x07, 2, val); }
+void L6470::setparam_minspeed(long val) { this->transfer(0x08, 2, val); }
+void L6470::setparam_fsspd(long val) { this->transfer(0x15, 2, val); }
+void L6470::setparam_kvalhold(long val) { this->transfer(0x09, 1, val); }
+void L6470::setparam_kvalrun(long val) { this->transfer(0x0a, 1, val); }
+void L6470::setparam_kvalacc(long val) { this->transfer(0x0b, 1, val); }
+void L6470::setparam_kvaldec(long val) { this->transfer(0x0c, 1, val); }
+void L6470::setparam_intspd(long val) { this->transfer(0x0d, 2, val); }
+void L6470::setparam_stslp(long val) { this->transfer(0x0e, 1, val); }
+void L6470::setparam_fnslpacc(long val) { this->transfer(0x0f, 1, val); }
+void L6470::setparam_fnslpdec(long val) { this->transfer(0x10, 1, val); }
+void L6470::setparam_ktherm(long val) { this->transfer(0x11, 1, val); }
+void L6470::setparam_ocdth(long val) { this->transfer(0x13, 1, val); }
+void L6470::setparam_stallth(long val) { this->transfer(0x14, 1, val); }
+void L6470::setparam_stepmood(long val) { this->transfer(0x16, 1, val); }
+void L6470::setparam_alareen(long val) { this->transfer(0x17, 1, val); }
+void L6470::setparam_config(long val) { this->transfer(0x18, 2, val); }
+
+long L6470::getparam_abspos() { return this->get_param(0x01, 3); }
+long L6470::getparam_elpos() { return this->get_param(0x02, 2); }
+long L6470::getparam_mark() { return this->get_param(0x03, 3); }
+long L6470::getparam_speed() { return this->get_param(0x04, 3); }
+long L6470::getparam_acc() { return this->get_param(0x05, 2); }
+long L6470::getparam_dec() { return this->get_param(0x06, 2); }
+long L6470::getparam_maxspeed() { return this->get_param(0x07, 2); }
+long L6470::getparam_minspeed() { return this->get_param(0x08, 2); }
+long L6470::getparam_fsspd() { return this->get_param(0x15, 2); }
+long L6470::getparam_kvalhold() { return this->get_param(0x09, 1); }
+long L6470::getparam_kvalrun() { return this->get_param(0x0a, 1); }
+long L6470::getparam_kvalacc() { return this->get_param(0x0b, 1); }
+long L6470::getparam_kvaldec() { return this->get_param(0x0c, 1); }
+long L6470::getparam_intspd() { return this->get_param(0x0d, 2); }
+long L6470::getparam_stslp() { return this->get_param(0x0e, 1); }
+long L6470::getparam_fnslpacc() { return this->get_param(0x0f, 1); }
+long L6470::getparam_fnslpdec() { return this->get_param(0x10, 1); }
+long L6470::getparam_ktherm() { return this->get_param(0x11, 1); }
+long L6470::getparam_adcout() { return this->get_param(0x12, 1); }
+long L6470::getparam_ocdth() { return this->get_param(0x13, 1); }
+long L6470::getparam_stallth() { return this->get_param(0x14, 1); }
+long L6470::getparam_stepmood() { return this->get_param(0x16, 1); }
+long L6470::getparam_alareen() { return this->get_param(0x17, 1); }
+long L6470::getparam_config() { return this->get_param(0x18, 2); }
+long L6470::getparam_status() { return this->get_param(0x19, 2); }
+
+
+L6470::L6470(const int ss_pin, const int mosi_pin, const int miso_pin, const int sck_pin,
+             const int busy_pin, const int alarm_pin, const int reset_pin) {
+  _ss_pin = ss_pin;
+  _mosi_pin = mosi_pin;
+  _miso_pin = miso_pin;
+  _sck_pin = sck_pin;
+  _busy_pin = busy_pin;
+  _alarm_pin = alarm_pin;
+  _reset_pin = reset_pin;
+}
+
+void L6470::init() {
+    pinMode(_ss_pin, OUTPUT);
+	pinMode(_mosi_pin, OUTPUT);
+	pinMode(_miso_pin, INPUT);
+	pinMode(_sck_pin, OUTPUT);
+	pinMode(_alarm_pin, INPUT);
+	pinMode(_reset_pin, OUTPUT);
+
+	digitalWrite(_ss_pin, HIGH);
+	//	
+	SPI.begin();
+	SPI.setDataMode(SPI_MODE3);
+	SPI.setClockDivider(SPI_CLOCK_DIV16);
+	SPI.setBitOrder(MSBFIRST);
+}
+
+void L6470::run(int dia, long spd)
+{
+    if (dia == 1)
+        this->transfer(0x51, 3, spd);
+    else
+        this->transfer(0x50, 3, spd);
+}
+
+void L6470::stepclock(int dia)
+{
+    if (dia == 1)
+        this->transfer(0x59, 0, 0);
+    else
+        this->transfer(0x58, 0, 0);
+}
+
+void L6470::move(int dia, long n_step)
+{
+    if (dia == 1)
+        this->transfer(0x41, 3, n_step);
+    else
+        this->transfer(0x40, 3, n_step);
+}
+
+void L6470::go_to(long pos)
+{
+    this->transfer(0x60, 3, pos);
+}
+
+void L6470::go_to_dia(int dia, int pos)
+{
+    if (dia == 1)
+        this->transfer(0x69, 3, pos);
+    else
+        this->transfer(0x68, 3, pos);
+}
+
+void L6470::go_until(int act, int dia, long spd)
+{
+    if (act == 1)
+        if (dia == 1)
+            this->transfer(0x8b, 3, spd);
+        else
+            this->transfer(0x8a, 3, spd);
+    else if (dia == 1)
+        this->transfer(0x83, 3, spd);
+    else
+        this->transfer(0x82, 3, spd);
+}
+
+void L6470::release_sw(int act, int dia)
+{
+    if (act == 1)
+        if (dia == 1)
+            this->transfer(0x9b, 0, 0);
+        else
+            this->transfer(0x9a, 0, 0);
+    else if (dia == 1)
+        this->transfer(0x93, 0, 0);
+    else
+        this->transfer(0x92, 0, 0);
+}
+
+void L6470::go_home()
+{
+    this->transfer(0x70, 0, 0);
+}
+
+void L6470::go_mark()
+{
+    this->transfer(0x78, 0, 0);
+}
+
+void L6470::reset_pos()
+{
+    this->transfer(0xd8, 0, 0);
+}
+
+void L6470::reset_device()
+{
+    this->send_u(0x00); //nop命令
+    this->send_u(0x00);
+    this->send_u(0x00);
+    this->send_u(0x00);
+    this->send_u(0xc0);
+}
+
+void L6470::softstop()
+{
+    this->transfer(0xb0, 0, 0);
+}
+
+void L6470::hardstop()
+{
+    this->transfer(0xb8, 0, 0);
+}
+
+void L6470::softhiz()
+{
+    this->transfer(0xa0, 0, 0);
+}
+
+void L6470::hardhiz()
+{
+    this->transfer(0xa8, 0, 0);
+}
+
+long L6470::get_status()
+{
+    long val = 0;
+    this->send_u(0xd0);
+    for (int i = 0; i <= 1; i++)
+    {
+        val = val << 8;
+        digitalWrite(PIN_SPI_SS, LOW);
+        val = val | SPI.transfer(0x00);
+        digitalWrite(PIN_SPI_SS, HIGH);
+    }
+    return val;
+}
+
+void L6470::transfer(int add, int bytes, long val)
+{
+    int data[3];
+    this->send(add);
+    for (int i = 0; i <= bytes - 1; i++)
+    {
+        data[i] = val & 0xff;
+        val = val >> 8;
+    }
+    if (bytes == 3)
+    {
+        this->send(data[2]);
+    }
+    if (bytes >= 2)
+    {
+        this->send(data[1]);
+    }
+    if (bytes >= 1)
+    {
+        this->send(data[0]);
+    }
+}
+
+void L6470::send(unsigned char add_or_val)
+{
+    while (!digitalRead(_busy_pin))
+    {
+    } //BESYが解除されるまで待機
+
+    digitalWrite(PIN_SPI_SS, LOW);
+    SPI.transfer(add_or_val);
+    digitalWrite(PIN_SPI_SS, HIGH);
+}
+
+//busyを確認せず送信
+void L6470::send_u(unsigned char add_or_val)
+{                                   
+    digitalWrite(PIN_SPI_SS, LOW);
+    SPI.transfer(add_or_val);
+    digitalWrite(PIN_SPI_SS, HIGH);
+}
+
+void L6470::busy_delay(long time)
+{ 
+    //BESYが解除されるまで待機
+    while (!digitalRead(_busy_pin))
+    {
+    }
+    delay(time);
+}
+
+long L6470::get_param(int add, int bytes)
+{
+    long val = 0;
+    int send_add = add | 0x20;
+    this->send_u(send_add);
+    for (int i = 0; i <= bytes - 1; i++)
+    {
+        val = val << 8;
+        digitalWrite(PIN_SPI_SS, LOW);
+        val = val | SPI.transfer(0x00);
+        digitalWrite(PIN_SPI_SS, HIGH);
+    }
+    return val;
+}
---
+++ StepperL6470.h
@@ -0,0 +1,113 @@
+#ifndef _L6470_H_
+#define _L6470_H_
+
+class L6470 {
+
+    public:
+    L6470(const int ss_pin, const int mosi_pin, const int miso_pin, const int sck_pin, 
+          const int busy_pin, const int alarm_pin, const int reset_pin); 
+    void init();
+
+    /* 
+    レジスタ操作　
+    [R]:読み取り専用
+    [WR]:いつでも書き換え可
+    [WH]:書き込みは出力がハイインピーダンスの時のみ可
+    [WS]:書き換えはモータが停止している時のみ可
+    */
+    // 書き込み
+    void setparam_abspos(long val); //[R, WS]現在座標default 0x000000 (22bit) 
+    void setparam_elpos(long val); //[R, WS]コイル励磁の電気的位置default 0x000 (2+7bit)
+    void setparam_mark(long val); //[R, WR]マーク座標default 0x000000 (22bit)
+    void setparam_acc(long val); //[R, WS] 加速度default 0x08A (12bit) (14.55*val+14.55[step/s^2])
+    void setparam_dec(long val); //[R, WS] 減速度default 0x08A (12bit) (14.55*val+14.55[step/s^2])
+    void setparam_maxspeed(long val); //[R, WR]最大速度default 0x041 (10bit) (15.25*val+15.25[step/s])
+    void setparam_minspeed(long val); //[R, WS]最小速度default 0x000 (1+12bit) (0.238*val+[step/s])
+    void setparam_fsspd(long val); //[R, WR]μステップからフルステップへの切替点速度default 0x027 (10bit) (15.25*val+7.63[step/s])
+    void setparam_kvalhold(long val); //[R, WR]停止時励磁電圧default 0x29 (8bit) (Vs[V]*val/256)
+    void setparam_kvalrun(long val); //[R, WR]定速回転時励磁電圧default 0x29 (8bit) (Vs[V]*val/256)
+    void setparam_kvalacc(long val); //[R, WR]加速時励磁電圧default 0x29 (8bit) (Vs[V]*val/256)
+    void setparam_kvaldec(long val); //[R, WR]減速時励磁電圧default 0x29 (8bit) (Vs[V]*val/256)
+    void setparam_intspd(long val); //[R, WH]逆起電力補償切替点速度default 0x0408 (14bit) (0.238*val[step/s])
+    void setparam_stslp(long val); //[R, WH]逆起電力補償低速時勾配default 0x19 (8bit) (0.000015*val[% s/step])
+    void setparam_fnslpacc(long val); //[R, WH]逆起電力補償高速時加速勾配default 0x29 (8bit) (0.000015*val[% s/step])
+    void setparam_fnslpdec(long val); //[R, WH]逆起電力補償高速時減速勾配default 0x29 (8bit) (0.000015*val[% s/step])
+    void setparam_ktherm(long val); //[R, WR]不明default 0x0 (4bit) (0.03125*val+1)
+    void setparam_ocdth(long val); //[R, WR]過電流しきい値default 0x8 (4bit) (375*val+375[mA])
+    void setparam_stallth(long val); //[R, WR]失速電流しきい値？default 0x40 (7bit) (31.25*val+31.25[mA])
+    void setparam_stepmood(long val); //[R, WH]ステップモードdefault 0x07 (1+3+1+3bit)
+    void setparam_alareen(long val); //[R, WS]有効アラームdefault 0xff (1+1+1+1+1+1+1+1bit)
+    void setparam_config(long val); //[R, WH]各種設定default 0x2e88 (3+3+2+1+1+1+1+1+3bit)
+
+    // 読み取り
+    long getparam_abspos();
+    long getparam_elpos();
+    long getparam_mark();
+    long getparam_speed();
+    long getparam_acc();
+    long getparam_dec();
+    long getparam_maxspeed();
+    long getparam_minspeed();
+    long getparam_fsspd();
+    long getparam_kvalhold();
+    long getparam_kvalrun();
+    long getparam_kvalacc();
+    long getparam_kvaldec();
+    long getparam_intspd();
+    long getparam_stslp();
+    long getparam_fnslpacc();
+    long getparam_fnslpdec();
+    long getparam_ktherm();
+    long getparam_adcout();
+    long getparam_ocdth();
+    long getparam_stallth();
+    long getparam_stepmood();
+    long getparam_alareen();
+    long getparam_config();
+    long getparam_status();
+
+    /*
+    コントロール
+    dia   1:正転 0:逆転,
+    spd  (20bit)(0.015*spd[step/s])
+    pos  (22bit)
+    n_step (22bit)
+    act   1:絶対座標をマーク  0:絶対座標リセット
+    mssec ミリ秒
+    val 各レジスタに書き込む値
+    */
+    void run(int dia, long spd); //指定方向に連続回転
+    void stepclock(int dia); //指定方向にstepピンのクロックで回転
+    void move(int dia, long n_step); //指定方向に指定数ステップする 
+    void go_to(long pos); //指定座標に最短でいける回転方向で移動
+    void go_to_dia(int dia, int pos); //回転方向を指定して指定座標に移動
+    void go_until(int act, int dia, long spd); //指定した回転方向に指定した速度で回転し、スイッチのONで急停止と座標処理
+    void release_sw(int act, int dia); //スイッチがOFFに戻るまで最低速度で回転し、停止と座標処理
+    void go_home(); //座標原点に移動
+    void go_mark();　//マーク座標に移動
+    void reset_pos();　//絶対座標リセット
+    void reset_device(); //L6470リセット
+    void softstop();　//回転停止、保持トルクあり
+    void hardstop();　//回転急停止、保持トルクあり
+    void softhiz(); //回転停止、保持トルクなし
+    void hardhiz(); //回転急停止、保持トルクなし
+    long get_status(); //statusレジスタの値を返す （this->get_param_status();と同じ）
+    void busy_delay(long time);　//busyフラグがHIGHになってから、指定ミリ秒待つ。
+
+    void transfer(int add, int bytes, long val);
+    void send(unsigned char add_or_val);
+    void send_u(unsigned char add_or_val);
+    long get_param(int add, int bytes);
+
+    private:
+    int _ss_pin;
+    int _mosi_pin;
+    int _miso_pin;
+    int _sck_pin;
+    int _busy_pin;
+    int _alarm_pin;
+    int _reset_pin;
+};
+
+#endif
+
